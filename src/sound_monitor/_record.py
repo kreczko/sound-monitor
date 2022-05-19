4ecc8802307ff7a06d02d5d2a1278712751abd74
kreczko <lkreczko@googlemail.com>
2022-05-19 16:57:26 +0100

    re-organized recording: RecordingSettings, split file writing

--- src/sound_monitor/_record.py
+++ src/sound_monitor/_record.py
@@ -1,37 +1,63 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
+
 import pyaudio
 import wave
 
 DEFAULT_CHUNK_SIZE = 8192
+DEFAULT_SAMPLE_FORMAT = pyaudio.paInt16  # 16 bits per sample
+DEFAULT_CHANNELS = 1
+DEFAULT_RATE = 44100  # Hz
+DEFAULT_RECORDING_TIME = 3  # seconds
 
 
-def record() -> None:
-    # example from https://realpython.com/playing-and-recording-sound-python/
-    chunk = 1024  # Record in chunks of 1024 samples
-    sample_format = pyaudio.paInt16  # 16 bits per sample
-    channels = 2
-    fs = 44100  # Record at 44100 samples per second
-    seconds = 3
-    filename = "output.wav"
+@dataclass
+class RecordingSettings:
+    chunk_size: int = DEFAULT_CHUNK_SIZE
+    sample_format: int = DEFAULT_SAMPLE_FORMAT
+    channels: int = DEFAULT_CHANNELS
+    rate_in_Hz: int = DEFAULT_RATE
+    recording_time_in_s: int = DEFAULT_RECORDING_TIME
+
+
+def _save_to_wave(
+    settings: RecordingSettings,
+    sample_size: int,
+    output_file_name: str,
+    frames: list[bytes],
+) -> None:
+    # Save the recorded data as a WAV file
+    wf = wave.open(output_file_name, "wb")
+    wf.setnchannels(settings.channels)
+    wf.setsampwidth(sample_size)
+    wf.setframerate(settings.rate_in_Hz)
+    wf.writeframes(b"".join(frames))
+    wf.close()
+
+
+def record(
+    settings: RecordingSettings,
+    output_file_name: str = "recording.wav",
+) -> None:
 
     p = pyaudio.PyAudio()  # Create an interface to PortAudio
 
-    # print("Recording")
-
     stream = p.open(
-        format=sample_format,
-        channels=channels,
-        rate=fs,
-        frames_per_buffer=chunk,
+        format=settings.sample_format,
+        channels=settings.channels,
+        rate=settings.rate_in_Hz,
+        frames_per_buffer=settings.chunk_size,
         input=True,
     )
 
-    frames = []  # Initialize array to store frames
+    frames: list[bytes] = []  # Initialize array to store frames
 
     # Store data in chunks for 3 seconds
-    for _ in range(0, int(fs / chunk * seconds)):
-        data = stream.read(chunk)
+    for _ in range(
+        0, int(settings.rate_in_Hz / settings.chunk_size * settings.recording_time_in_s)
+    ):
+        data = stream.read(settings.chunk_size)
         frames.append(data)
 
     # Stop and close the stream
@@ -41,11 +67,9 @@ def record() -> None:
     p.terminate()
 
     # print("Finished recording")
-
-    # Save the recorded data as a WAV file
-    wf = wave.open(filename, "wb")
-    wf.setnchannels(channels)
-    wf.setsampwidth(p.get_sample_size(sample_format))
-    wf.setframerate(fs)
-    wf.writeframes(b"".join(frames))
-    wf.close()
+    _save_to_wave(
+        settings=settings,
+        sample_size=p.get_sample_size(settings.sample_format),
+        output_file_name=output_file_name,
+        frames=frames,
+    )
--- src/sound_monitor/cli.py
+++ src/sound_monitor/cli.py
@@ -21,15 +21,62 @@ def version() -> None:
 
 @app.command()
 def record(
-    chunk_size: int
-    | None = typer.Option(
+    chunk_size: int = typer.Option(
         _record.DEFAULT_CHUNK_SIZE,
         "--chunk-size",
         "-c",
         help="Chunk size to read from audio stream",
     ),
+    sample_format: int = typer.Option(
+        _record.DEFAULT_SAMPLE_FORMAT,
+        "--sample-format",
+        "-s",
+        help="Sample format to use",
+    ),
+    channels: int = typer.Option(
+        _record.DEFAULT_CHANNELS,
+        "--channels",
+        "-n",
+        help="Number of channels to use",
+    ),
+    rate_in_Hz: int = typer.Option(
+        _record.DEFAULT_RATE,
+        "--rate",
+        "-r",
+        help="Sample rate to use",
+    ),
+    recording_time_in_s: int = typer.Option(
+        _record.DEFAULT_RECORDING_TIME,
+        "--recording-time",
+        "-t",
+        help="Recording time in seconds",
+    ),
+    output_file_name: str = typer.Option(
+        "recording.wav",
+        "--output",
+        "-o",
+        help="Output file name",
+    ),
 ) -> None:
-    _record.record()
+    settings = _record.RecordingSettings(
+        chunk_size=chunk_size,
+        sample_format=sample_format,
+        channels=channels,
+        rate_in_Hz=rate_in_Hz,
+        recording_time_in_s=recording_time_in_s,
+    )
+    # TODO:
+    # Change output_file_name to a output directory
+    # save settings to a JSON file
+    # record until the user presses Ctrl-C
+    # save the recording to a set of WAV files
+    # file format: <output dir>/<timestamp>_<sensor_id>.wav
+    # with
+    # timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%I_%M_%S_%f")
+    _record.record(
+        settings=settings,
+        output_file_name=output_file_name,
+    )
 
 
 @app.command()
